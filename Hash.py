e207e75e10c2ecf34e0f141a0f7b80f8b4d0bdcd
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-01-22 22:35:23 -0500

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -16,6 +16,7 @@ class Excel:
         sheet.merge_cells('A1:C1')
         sheet.merge_cells('D1:G1')
         sheet.merge_cells('H1:O1')
+        
         # Add Header
         sheet['A1'] = "Binary Value"
         sheet['D1'] = "MD5"
@@ -29,9 +30,11 @@ class Excel:
         sheet['V1'] = "MD5 Standard Deviation"
         sheet['W1'] = "SHA256 Standard Deviation"
 
-        # Add Data
+        # Arrays to hold hashes converter to decimal
         md5_decimals = []
         sha256_decimals = []
+        
+        # Add Data
         for row_index, value in enumerate(data, start=2):
             md5_hash = Hash.calculate_hash_md5(value)
             sha256_hash = Hash.calculate_sha256(value)
@@ -43,14 +46,15 @@ class Excel:
             sheet.cell(row=row_index, column=8, value=sha256_hash)
             sheet.cell(row=row_index, column=16, value=md5_decimal)
             sheet.cell(row=row_index, column=17, value=sha256_decimal)
-
-            md5_decimals.append(md5_decimal) # Store MD5 Decimal values
+            
+            # Store MD5 Decimal values
+            md5_decimals.append(md5_decimal) 
             md5_differences = [md5_decimals[i] - md5_decimals[i - 1] for i in range(1, len(md5_decimals))]
             for row_index, difference in enumerate(md5_differences, start=2):
                 sheet.cell(row=row_index, column=18, value=difference)
                 
-                
-            sha256_decimals.append(sha256_decimal) # Store SHA256 Decimal values
+            # Store SHA256 Decimal values   
+            sha256_decimals.append(sha256_decimal) 
             sha256_differences = [sha256_decimals[i] - sha256_decimals[i - 1] for i in range(1, len(sha256_decimals))]
             for row_index, difference in enumerate(sha256_differences, start=2):
                 sheet.cell(row=row_index, column=19, value=difference)
